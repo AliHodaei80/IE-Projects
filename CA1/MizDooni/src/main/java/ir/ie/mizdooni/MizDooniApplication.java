30f0bc045665244c7f6a7cfe491c7c212242e89c
ali <ali.n.hodaei@gmail.com>
2024-02-20 22:57:40 +0330

    feat(init): initial structure created.

---
+++ CA1/MizDooni/src/main/java/ir/ie/mizdooni/MizDooniApplication.java
@@ -0,0 +1,11 @@
+package ir.ie.mizdooni;
+
+
+
+public class MizDooniApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ CA1/MizDooni/src/test/java/ir/ie/mizdooni/MizDooniApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.ie.mizdooni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MizDooniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
