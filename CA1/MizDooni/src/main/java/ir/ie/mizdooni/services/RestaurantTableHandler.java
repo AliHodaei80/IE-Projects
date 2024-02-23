e85f9fd5105e4e20d9e26201ecb70d06044c97eb
pooriataj <pouriyatajmehrabi1381@gmail.com>
2024-02-23 12:56:52 +0330

    fix: add restarant table handler singleton inner code

--- CA1/MizDooni/src/main/java/ir/ie/mizdooni/services/RestaurantTableHandler.java
+++ CA1/MizDooni/src/main/java/ir/ie/mizdooni/services/RestaurantTableHandler.java
@@ -1,14 +1,22 @@
 package ir.ie.mizdooni.services;
 
+import ir.ie.mizdooni.storage.RestaurantTables;
+import ir.ie.mizdooni.storage.Restaurants;
+
 public class RestaurantTableHandler {
 
     private static RestaurantTableHandler restaurantTableHandler;
-    private RestaurantTableHandler() {
+    private final Restaurants restaurants;
+    private final RestaurantTables restauarantTables;
+
+    private RestaurantTableHandler(Restaurants rests) {
+        restaurants = rests;
+        restauarantTables = new RestaurantTables();
     }
 
-    public static RestaurantTableHandler getInstance() {
+    public static RestaurantTableHandler getInstance(Restaurants rests) {
         if (restaurantTableHandler == null)
-            restaurantTableHandler = new RestaurantTableHandler();
+            restaurantTableHandler = new RestaurantTableHandler(rests);
         return restaurantTableHandler;
     }
 }
