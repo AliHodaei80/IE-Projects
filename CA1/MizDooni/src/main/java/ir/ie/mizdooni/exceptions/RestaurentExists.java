4f313982de7b3c3fdbb5115bdc1cbbd953d19dd7
pooriataj <pouriyatajmehrabi1381@gmail.com>
2024-02-23 13:10:07 +0330

    fix: add testcases and add another missing java package

---
+++ CA1/MizDooni/src/main/java/ir/ie/mizdooni/exceptions/RestaurentExists.java
@@ -0,0 +1,11 @@
+package ir.ie.mizdooni.exceptions;
+
+import static ir.ie.mizdooni.defines.Errors.REST_M_NOT_FOUND;
+import static ir.ie.mizdooni.defines.Errors.REST_NAME_NOT_UNIQUE;
+
+public class RestaurentExists extends Exception {
+    public RestaurentExists() {
+        super(REST_NAME_NOT_UNIQUE);
+    }
+
+}
