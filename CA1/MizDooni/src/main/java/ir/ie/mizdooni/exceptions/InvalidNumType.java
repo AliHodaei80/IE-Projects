51fb9115105b8fdb02b3201c44dfb32ea1e3cb8b
ali <ali.n.hodaei@gmail.com>
2024-02-23 19:19:12 +0330

    fix: check seat number be natural number validation and validatin reserve table request added.

---
+++ CA1/MizDooni/src/main/java/ir/ie/mizdooni/exceptions/InvalidNumType.java
@@ -0,0 +1,10 @@
+package ir.ie.mizdooni.exceptions;
+
+import static ir.ie.mizdooni.defines.Errors.INVALID_NUM_TYPE;
+
+public class InvalidNumType extends Exception {
+    public InvalidNumType() {
+        super(INVALID_NUM_TYPE);
+    }
+
+}
--- CA1/MizDooni/src/main/java/ir/ie/mizdooni/validators/RequestSchemaValidator.java
+++ CA1/MizDooni/src/main/java/ir/ie/mizdooni/validators/RequestSchemaValidator.java
@@ -1,11 +1,9 @@
 package ir.ie.mizdooni.validators;
 
 import ir.ie.mizdooni.commons.Request;
-import ir.ie.mizdooni.exceptions.InvalidEmailFormat;
-import ir.ie.mizdooni.exceptions.InvalidRequestFormat;
-import ir.ie.mizdooni.exceptions.InvalidTimeFormat;
-import ir.ie.mizdooni.exceptions.InvalidUsernameFormat;
+import ir.ie.mizdooni.exceptions.*;
 
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
@@ -16,6 +14,7 @@ import java.util.regex.Pattern;
 
 import static ir.ie.mizdooni.defines.Commands.*;
 import static ir.ie.mizdooni.defines.RequestKeys.*;
+import static ir.ie.mizdooni.defines.TimeFormats.RESERVE_DATETIME_FORMAT;
 import static ir.ie.mizdooni.defines.TimeFormats.RESTAURANT_TIME_FORMAT;
 
 public class RequestSchemaValidator {
@@ -25,6 +24,8 @@ public class RequestSchemaValidator {
             START_TIME_KEY, END_TIME_KEY, RESTAURANT_ADDRESS_KEY, DESCRIPTION_KEY, RESTAURANT_TYPE_KEY);
     final static List<String> tableAdditionKeys = Arrays.asList(TABLE_NUM_KEY, RESTAURANT_NAME_KEY,
             MANAGER_USERNAME_KEY, SEATS_NUM_KEY);
+    final static List<String> tableAReserveKeys = Arrays.asList(USERNAME_KEY, RESTAURANT_NAME_KEY,
+            TABLE_NUM_KEY, DATETIME_KEY);
     final static List<String> userAdditionAddressKeys = Arrays.asList(CITY_KEY, COUNTRY_KEY);
     final static List<String> restAdditionAddressKeys = Arrays.asList(CITY_KEY, COUNTRY_KEY, STREET_KEY);
 
@@ -35,6 +36,11 @@ public class RequestSchemaValidator {
                 throw new InvalidRequestFormat(key);
             }
         }
+    }
+
+    public static void checkBeNaturalNumber(double num) throws InvalidNumType {
+        if (num != (int) num && num >= 1)
+            throw new InvalidNumType();
     }
 
     public static boolean patternMatches(String str, String regexPattern) {
@@ -54,6 +60,15 @@ public class RequestSchemaValidator {
         try {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern(RESTAURANT_TIME_FORMAT);
             LocalTime.parse(timeString, formatter);
+        } catch (DateTimeParseException e) {
+            throw new InvalidTimeFormat(RESTAURANT_TIME_FORMAT);
+        }
+    }
+
+    public static void validateDateTime(String dateTimeString) throws InvalidTimeFormat {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(RESERVE_DATETIME_FORMAT);
+            LocalDateTime.parse(dateTimeString, formatter);
         } catch (DateTimeParseException e) {
             throw new InvalidTimeFormat(RESTAURANT_TIME_FORMAT);
         }
@@ -84,13 +99,19 @@ public class RequestSchemaValidator {
         validateTime((String) data.get(START_TIME_KEY));
     }
 
-    public static void validateAddTable(Map<String, Object> data) throws InvalidTimeFormat, InvalidRequestFormat {
+    public static void validateAddTable(Map<String, Object> data) throws InvalidTimeFormat, InvalidRequestFormat, InvalidNumType {
         checkKeyInclusion(data, tableAdditionKeys);
+        checkBeNaturalNumber((double) data.get(SEATS_NUM_KEY));
+    }
+
+    public static void validateReserveTable(Map<String, Object> data) throws InvalidTimeFormat, InvalidRequestFormat {
+        checkKeyInclusion(data, tableAReserveKeys);
+        validateDateTime((String) data.get(DATETIME_KEY));
     }
 
     // TODO add validate request for adding restaurant table
     public static void validate(Request r)
-            throws InvalidTimeFormat, InvalidUsernameFormat, InvalidRequestFormat, InvalidEmailFormat {
+            throws InvalidTimeFormat, InvalidUsernameFormat, InvalidRequestFormat, InvalidEmailFormat, InvalidNumType {
         String op = r.getOperation();
         Map<String, Object> data = r.getData();
         switch (op) {
@@ -102,6 +123,9 @@ public class RequestSchemaValidator {
                 break;
             case OP_ADD_TABLE:
                 validateAddTable(data);
+                break;
+            case OP_RESERVE_TABLE:
+                validateReserveTable(data);
                 break;
         }
     }
