0d1681fa4cb2ca06df56881e5724978cc059af4c
pooriataj <pouriyatajmehrabi1381@gmail.com>
2024-02-23 12:43:45 +0330

    feat: Add storage for tables

--- CA1/MizDooni/src/main/java/ir/ie/mizdooni/storage/RestaurantTables.java
+++ CA1/MizDooni/src/main/java/ir/ie/mizdooni/storage/RestaurantTables.java
@@ -1,9 +1,21 @@
 package ir.ie.mizdooni.storage;
 
+import ir.ie.mizdooni.models.Restaurant;
 import ir.ie.mizdooni.models.RestaurantTable;
 
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Map;
 
 public class RestaurantTables {
     Map<String, Map<Integer, RestaurantTable>> restaurantTables;
+
+    public Collection<RestaurantTable> getRestTables(String restName) {
+        return restaurantTables.get(restName).values();
+    }
+
+    public void addRestaurantTable(String restName, String restManagerName, int tableNo, int seatsNo) {
+        restaurantTables.get(restName).put(tableNo, new RestaurantTable(seatsNo, restName, restManagerName, tableNo));
+    }
 }
