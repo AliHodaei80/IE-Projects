aa50e56a1e2abef73fc6bd70c9ecd3c9de96a23d
SyntheticDemon <pouriyatajmehrabi1381@gmail.com>
2024-02-24 19:44:26 +0330

    Fix some the messages

--- CA1/MizDooni/src/main/java/ir/ie/mizdooni/definitions/Errors.java
+++ CA1/MizDooni/src/main/java/ir/ie/mizdooni/definitions/Errors.java
@@ -17,11 +17,11 @@ public class Errors {
     public static final String RESTUARANT_NOT_FOUND = "Restaurant was not found";
     public static final String TABLE_NOT_FOUND = "Table was not found";
     public static final String INVALID_NUM_TYPE = "Number type is invalid";
-    public static final String TABLE_ALREADY_RESERVED = "The table already reserved";
-    public static final String INVALID_DATETIME = "The datetime is invalid. The datetime should be after current datetime";
+    public static final String TABLE_ALREADY_RESERVED = "The table is already reserved";
+    public static final String INVALID_DATETIME = "The datetime is invalid. The datetime should surpass current datetime";
     public static final String DATETIME_NOT_IN_RANGE = "The datetime should be in range of restaurant work time.";
-    public static final String RESERVATION_NOT_FOR_USER = "The reservation not belong to user";
-    public static final String CANCELLATION_TIME_PASSED = "The cancellation time passed";
+    public static final String RESERVATION_NOT_FOR_USER = "The reservation does not belong to user";
+    public static final String CANCELLATION_TIME_PASSED = "Cancellation Period has expired";
 
 
 }
