67aeaf522158414ad1299cb4cdeeb43e1fdae5c1
SyntheticDemon <pouriyatajmehrabi1381@gmail.com>
2024-04-29 23:59:37 +0330

    Rebuild the old home.js

--- CA4/front/src/Home.js
+++ CA4/front/src/Home.js
@@ -3,11 +3,22 @@ import AboutMizdooni from "./components/about.js";
 import SearchBarForm from "./components/searchbar.js";
 import Header from "./components/header.js";
 import Footer from "./components/footer.js";
+import logo_big from "./images/logo_big.png";
+import background_image from "./images/home.png";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Header />
-      <SearchBarForm />
+      <div class="container-s w-100 text-center">
+        <div class="home-background w-100" style={{backgroundImage: `url(${background_image})`}}>
+          <div class="container">
+            <div class="input-group mb-3">
+              <img class="big-logo" src={logo_big} />
+            </div>
+            <SearchBarForm />
+          </div>
+        </div>
+      </div>
       <AboutMizdooni />
       <Footer />
     </main>
