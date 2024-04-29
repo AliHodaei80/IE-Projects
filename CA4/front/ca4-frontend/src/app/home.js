07640691c65d1e924264a9bb7b31094fa2bf753a
SyntheticDemon <pouriyatajmehrabi1381@gmail.com>
2024-04-29 11:26:32 +0330

    add live server and static files

--- CA4/front/ca4-frontend/src/app/home.js
+++ CA4/front/ca4-frontend/src/app/home.js
@@ -1,22 +1,6 @@
 import Image from "next/image";
-import Link from "next/link";
-export default function Home() {
-  const images = [];
-  {
-    for (let index = 0; index < 5; index++) {
-      images.push(
-        <Image
-          src="/vercel.svg"
-          alt="Vercel Logo"
-          className="dark:invert"
-          width={100}
-          height={24}
-          priority
-        />
-      );
-    }
-  }
 
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -31,7 +15,15 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By {images}
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
           </a>
         </div>
       </div>
@@ -48,8 +40,8 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <Link
-          href="https://nextjs.org/docs"
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -63,7 +55,7 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Find in-depth information about Next.js features and API.
           </p>
-        </Link>
+        </a>
 
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
