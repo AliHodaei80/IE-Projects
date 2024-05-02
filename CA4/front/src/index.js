8fb6e183dd380884cd73364a794211e5ee42253b
SyntheticDemon <pouriyatajmehrabi1381@gmail.com>
2024-05-03 01:46:44 +0330

    fix: fix the Toast Container by moving it to the proper location to allow proper toastm essages

--- CA4/front/src/index.js
+++ CA4/front/src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import AboutMizdooni from "./components/about.js";
-import SearchBarForm from "./components/searchbar.js";
-import Home from './Home.js';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ToastContainer, toast } from "react-toastify";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+      <App />
+    <ToastContainer/>
   </React.StrictMode>
 );
