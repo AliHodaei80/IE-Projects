8e1e5879d300f230b837419e6c2386570bea05c5
SyntheticDemon <pouriyatajmehrabi1381@gmail.com>
2024-05-02 22:05:19 +0330

    Add succesfull login and sign up path

--- CA4/front/src/components/login.js
+++ CA4/front/src/components/login.js
@@ -2,11 +2,23 @@ import React, { useState } from "react";
 import Header from "./header.js";
 import Footer from "./footer.js";
 import "../styles/login_signup.css";
-import fetchData from "../utils/request_utils.jsx";
+import { fetchData, postData } from "../utils/request_utils.js";
+const base_path = "http://127.0.0.1:8080/";
+const login_path = "login";
+const signup_path = "signup";
 
 export default function Login() {
   const [showLogin, setShowLogin] = useState(true);
+  const [userData, setUserData] = useState({});
+  const [errorMessage, setErrorMessage] = useState({});
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUserData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
   return (
     <main>
       <header>
@@ -34,24 +46,26 @@ export default function Login() {
             </button>
           </div>
           {showLogin ? (
-            <div class="container">
-              <div class="row justify-content-center">
-                <div class="col-md-6 w-75">
-                  <div class="mt-4 w-100">
-                    <label class="form-label">Username</label>
+            <div className="container">
+              <div className="row justify-content-center">
+                <div className="col-md-6 w-75">
+                  <div className="mt-4 w-100">
+                    <label className="form-label">Username</label>
                     <input
                       type="text"
-                      class="form-control rounded-2"
+                      className="form-control rounded-2"
+                      onChange={handleChange}
                       name="username"
                       aria-label="Username"
                       aria-describedby="usernameHelp"
                     ></input>
                   </div>
-                  <div class="mt-4 w-100">
-                    <label class="form-label">Password</label>
+                  <div className="mt-4 w-100">
+                    <label className="form-label">Password</label>
                     <input
                       type="password"
-                      class="form-control rounded-2"
+                      onChange={handleChange}
+                      className="form-control rounded-2"
                       name="password"
                       aria-label="Password"
                       aria-describedby="passwordHelp"
@@ -59,7 +73,11 @@ export default function Login() {
                   </div>
                 </div>
                 <button
-                  className="btn btn-outline-secondary w-75"
+                  className="btn btn-outline-secondary w-75 mt-4"
+                  onClick={() => {
+                    console.log("Printing Error message ", errorMessage);
+                    setErrorMessage(postData(base_path + login_path, userData));
+                  }}
                   type="button"
                 >
                   Login
@@ -67,100 +85,107 @@ export default function Login() {
               </div>
             </div>
           ) : (
-            <div class="container">
-              <div class="row justify-content-center">
-                <div class="col-md-6 w-75">
-                  <div class="mt-4 w-100">
-                    <label class="form-label">Username</label>
+            <div className="container">
+              <div className="row justify-content-center">
+                <div className="col-md-6 w-75">
+                  <div className="mt-4 w-100">
+                    <label className="form-label">Username</label>
                     <input
                       type="text"
-                      class="form-control rounded-2"
+                      className="form-control rounded-2"
                       name="username"
+                      onChange={handleChange}
                       aria-label="Username"
                       aria-describedby="usernameHelp"
                     ></input>
                   </div>
-                  <div class="mt-4 w-100">
-                    <label class="form-label">Email</label>
+                  <div className="mt-4 w-100">
+                    <label className="form-label">Email</label>
                     <input
                       type="email"
-                      class="form-control rounded-2"
+                      className="form-control rounded-2"
                       name="email"
+                      onChange={handleChange}
                       aria-label="email"
                       aria-describedby="emailHelp"
                     ></input>
                   </div>
-                  <div class="mt-4 w-100">
-                    <label class="form-label">Country</label>
+                  <div className="mt-4 w-100">
+                    <label className="form-label">Country</label>
                     <input
                       type="text"
-                      class="form-control rounded-2"
+                      className="form-control rounded-2"
                       name="country"
+                      onChange={handleChange}
                       aria-label="country"
                       aria-describedby="countrydHelp"
                     ></input>
                   </div>
-                  <div class="mt-4 w-100">
-                    <label class="form-label">City</label>
+                  <div className="mt-4 w-100">
+                    <label className="form-label">City</label>
                     <input
                       type="text"
-                      class="form-control rounded-2"
+                      className="form-control rounded-2"
                       name="city"
+                      onChange={handleChange}
                       aria-label="city"
                       aria-describedby="cityHelp"
                     ></input>
                   </div>
-                  <div class="mt-4 w-100">
-                    <label class="form-label">Password</label>
+                  <div className="mt-4 w-100">
+                    <label className="form-label">Password</label>
                     <input
                       type="text"
-                      class="form-control rounded-2"
+                      className="form-control rounded-2"
                       name="password"
+                      onChange={handleChange}
                       aria-label="Password"
                       aria-describedby="passwordHelp"
                     ></input>
                   </div>
-                  <div class="d-flex mt-4 w-100 justify-content-between">
-                    <label class="form-label">I am a new </label>
+                  <div className="d-flex mt-4 w-100 justify-content-between">
+                    <label className="form-label">I am a new </label>
                     <div>
                       <input
-                        class="form-check-input"
+                        className="form-check-input"
                         type="radio"
-                        name="exampleRadios"
-                        id="exampleRadios1"
-                        value="Client"
-                        checked
+                        onChange={handleChange}
+                        name="role"
+                        id="radio_1"
+                        value="client"
                       ></input>
-                      <label class="ms-4 form-label">Client</label>
+                      <label className="ms-4 form-label">Client</label>
                     </div>
                     <div>
                       <input
-                        class="form-check-input"
+                        className="form-check-input"
                         type="radio"
-                        name="exampleRadios"
-                        id="exampleRadios1"
-                        value="Manager"
-                        checked
+                        onChange={handleChange}
+                        name="role"
+                        id="radio_2"
+                        value="manager"
                       ></input>
-                      <label class="ms-4 form-label">Manager</label>
+                      <label className="ms-4 form-label">Manager</label>
                     </div>
                   </div>
                 </div>
-                {showLogin ? (
-                  <button
-                    className="btn  btn-outline-secondary w-75"
-                    type="button"
-                  >
-                    Login
-                  </button>
-                ) : (
-                  <button
-                    className="btn btn-outline-secondary w-75"
-                    type="button"
-                  >
-                    Signup
-                  </button>
-                )}
+                <button
+                  type="button"
+                  id="signup_button"
+                  className="btn btn-outline-secondary w-75 mt-4"
+                  onClick={() => {
+                    console.log("Printing Error message ", errorMessage);
+                    const newUserData = userData;
+                    newUserData.address = {};
+                    newUserData.address.city = userData.city;
+                    newUserData.address.country = userData.country;
+                    setErrorMessage(
+                      postData(base_path + signup_path, newUserData)
+                    );
+                  }}
+                >
+                  Signup
+                </button>
               </div>
             </div>
           )}
