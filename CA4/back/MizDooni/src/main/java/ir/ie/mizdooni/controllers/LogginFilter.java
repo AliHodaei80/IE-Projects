d6eb6cbd45bd40733eeaac68d45455afd00b0886
ali <ali.n.hodaei@gmail.com>
2024-05-04 23:20:59 +0330

    fix: modify filter to allow for restaurant path and restaurant search

--- CA4/back/MizDooni/src/main/java/ir/ie/mizdooni/controllers/LogginFilter.java
+++ CA4/back/MizDooni/src/main/java/ir/ie/mizdooni/controllers/LogginFilter.java
@@ -9,16 +9,18 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
-import static ir.ie.mizdooni.definitions.Paths.LOGIN_URL;
-import static ir.ie.mizdooni.definitions.Paths.SIGNUP_URL;
+import static ir.ie.mizdooni.definitions.Paths.*;
 
 public class LogginFilter implements Filter {
     private static final Logger logger = LoggerFactory.getLogger(LogginFilter.class);
+
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         logger.info("LogginFilter");
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
-        if (req.getRequestURI().equals(LOGIN_URL) || req.getRequestURI().equals(SIGNUP_URL)) {
+        if (req.getRequestURI().equals(LOGIN_URL) || req.getRequestURI().equals(SIGNUP_URL) ||
+                req.getRequestURI().equals(RESTAURANTS_SEARCH_URL) || req.getRequestURI().equals(RESTAURANTS_URL) ||
+                req.getRequestURI().matches(RESTAURANT_URL_REGEX)) {
             chain.doFilter(request, response);
             return;
         }
--- CA4/back/MizDooni/src/main/java/ir/ie/mizdooni/definitions/Paths.java
+++ CA4/back/MizDooni/src/main/java/ir/ie/mizdooni/definitions/Paths.java
@@ -3,13 +3,15 @@ package ir.ie.mizdooni.definitions;
 public class Paths {
     public static final String LOGIN_URL = "/login";
     public static final String SIGNUP_URL = "/signup";
+    public static final String RESTAURANT_URL_REGEX = "^/restaurant/\\d+$";
+    public static final String RESTAURANTS_URL = "/restaurants";
+    public static final String RESTAURANTS_SEARCH_URL = "/restaurants/search";
 
     public static final String CLIENT_HOME_JSP_PAGE = "client_home.jsp";
     public static final String MANAGER_HOME_JSP_PAGE = "manager_home.jsp";
     public static final String ERROR_JSP_PAGE = "/error.jsp";
     public static final String LOGOUT_PAGE = "/logout";
-    public static final String RESTAURANTS_PAGE = "/restaurants";
-    public static final String RESTAURANT_PAGE = "/restaurants/*";
+    public static final String RESTAURANT_PAGE = "/restaurants";
     public static final String RESERVATIONS_PAGE = "/reservations";
 
 }
