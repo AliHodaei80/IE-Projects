40852536bd4f4e6eea2babf46bd00c8c58a0762b
SyntheticDemon <pouriyatajmehrabi1381@gmail.com>
2024-05-05 15:15:08 +0330

    feature : add Local storage fetch for the context, fix some issues with the header and add new navigation logic

--- CA4/front/src/context/AuthContext.js
+++ CA4/front/src/context/AuthContext.js
@@ -1,12 +1,20 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [authDetails, setAuthDetails] = useState({ logged_in: false });
+  const [authDetails, setAuthDetails] = useState(() => {
+    const savedAuthDetails = localStorage.getItem("authDetails");
+    return savedAuthDetails
+      ? JSON.parse(savedAuthDetails)
+      : { logged_in: false };
+  });
+  useEffect(() => {
+    localStorage.setItem("authDetails", JSON.stringify(authDetails));
+  }, [authDetails]);
 
   return (
-    <AuthContext.Provider value={{authDetails, setAuthDetails}}>
+    <AuthContext.Provider value={{ authDetails, setAuthDetails }}>
       {children}
     </AuthContext.Provider>
   );
